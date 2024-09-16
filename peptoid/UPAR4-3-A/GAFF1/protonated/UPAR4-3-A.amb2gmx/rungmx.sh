f6fcfd16dd1ce37352c62970800549a6e6f8139b
starwingc <tur54445@temple.edu>
2024-09-16 14:37:33 -0400

    prepare UPAR4-3-A/

---
+++ peptoid/UPAR4-3-A/GAFF1/protonated/UPAR4-3-A.amb2gmx/rungmx.sh
@@ -0,0 +1,6 @@
+
+echo 0 | gmx editconf -f UPAR4-3-A_GMX.gro -bt octahedron -d 10 -c -princ
+gmx grompp -f em.mdp -c out.gro -p UPAR4-3-A_GMX.top -o em.tpr -v
+gmx mdrun -ntmpi 1 -v -deffnm em
+gmx grompp -f md.mdp -c em.gro -p UPAR4-3-A_GMX.top -o md.tpr -r em.gro
+gmx mdrun -ntmpi 1 -v -deffnm md
